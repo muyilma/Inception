6dd2905f6b1cef80b259567d168c08e920a49288
Mustafa Emre Yılmaz <mustafaemreyilmz@gmail.com>
2026-09-01 21:41:57 +0300

    maridb .sh edited

--- inception/srcs/requirements/mariadb/tools/init_db.sh
+++ inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -2,29 +2,23 @@
 
 set -e
 
-echo "Starting MariaDB initialization..."
-
-mysqld --skip-networking  --user=mysql &
-pid="$!"
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+    echo "Starting MariaDB initialization..."
     
-echo "Waiting for MariaDB to be ready..."
-until mysqladmin  ping >/dev/null 2>&1; do
-    sleep 1
-done
-echo "MariaDB is ready!"
+    service mariadb start
 
-mysql  -u root << EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
+    mariadb -u root << EOF
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    FLUSH PRIVILEGES;
 EOF
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+    
+else
+    echo "MariaDB is already installed..."
+fi
 
-echo "Shutting down temporary MariaDB..."
-mysqladmin  -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
-
-wait "$pid" || true
-
-echo "Initialization complete. Starting MariaDB..."
-exec mysqld --user=mysql 
+echo "Initialization complete. Starting MariaDB in foreground..."
+exec mariadbd --user=mysql
