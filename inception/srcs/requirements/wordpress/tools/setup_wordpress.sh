db48a20b5effab52bf422b97c3e03872258492b2
Mustafa Emre Yılmaz <mustafaemreyilmz@gmail.com>
2026-09-03 16:21:48 +0300

    .conf edited

--- inception/srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ inception/srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -18,7 +18,7 @@ if [ ! -f "$WP_PATH/wp-config.php" ]; then
     echo "WordPress kuruluyor ve Admin hesabı yaratılıyor..."
     wp core install \
         --url=musyilma.42.fr \
-        --title="Inception Projesi" \
+        --title="Inception Project" \
         --admin_user=$WP_ADMIN \
         --admin_password=$WP_ADMIN_PASSWORD \
         --admin_email=$WP_ADMIN_EMAIL \
